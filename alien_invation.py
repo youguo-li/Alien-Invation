d2d48f31c9464d8d6c1d9b5ebd1b70e94c73cb30
youguo-art <youguo060225@gmail.com>
2025-12-03 15:42:31 +0800

    add py files

--- alien_invation.py
+++ alien_invation.py
@@ -1,11 +1,15 @@
 import sys
 import pygame
+from settings import Settings   
 
 class AlienInvation:
     """管理游戏资源和行为的类"""
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invation")
     
     def run_game(self):
@@ -15,8 +19,11 @@ class AlienInvation:
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     sys.exit()
+            # 每次循环时都重绘屏幕
+            self.screen.fill(self.settings.bg_color)
             #让最近绘制的屏幕可见
             pygame.display.flip()
+            self.clock.tick(60)
     
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
